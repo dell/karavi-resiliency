c9d70a40ce95f8ec0ee09266cb4001da68aa72c3
ameer-jabbar <ameer.jabbar@gmail.com>
2021-02-22 11:00:16 -0500

    Add copyright comments and OWNERs file

--- test/podmontest/ins.sh
+++ test/podmontest/ins.sh
@@ -1,4 +1,15 @@
 #!/bin
+#
+# Copyright (c) 2021. Dell Inc., or its subsidiaries. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+#
+
 instances="1 2 3 4"
 storageClassName=vxflexos-notopo
 image="$REGISTRY_HOST:$REGISTRY_PORT/podmontest:v0.0.54"
