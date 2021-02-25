0375b9659de7f2cae3c7b54ad1a67a73f98f03c3
rbo54 <thomas.watson@dell.com>
2021-02-25 18:05:57 -0500

    Add nodeSelector for a particular zone label on node

--- test/podmontest/insu.sh
+++ test/podmontest/insu.sh
@@ -2,6 +2,7 @@
 instances="1 2 3 4"
 ndevices=0
 nvolumes=4
+zone="zone213"
 storageClassName=unity-virt21048j9rzz-nfs
 image="$REGISTRY_HOST:$REGISTRY_PORT/podmontest:v0.0.54"
 port=5000
@@ -10,5 +11,5 @@ for i in $instances
 do
 	echo $i
 	kubectl create namespace pmtu$i
-        helm install -n "pmtu$i" "pmtu$i" deploy --values deploy/values-unity-nfs.yaml --set podmonTest.namespace="pmtu$i"  --set podmonTest.storageClassName="$storageClassName" --set podmonTest.ndevices=$ndevices --set podmonTest.nvolumes=$nvolumes --set podmonTest.image="$image" 
+        helm install -n "pmtu$i" "pmtu$i" deploy --values deploy/values-unity-nfs.yaml --set podmonTest.namespace="pmtu$i"  --set podmonTest.storageClassName="$storageClassName" --set podmonTest.ndevices=$ndevices --set podmonTest.nvolumes=$nvolumes ---set podmonTest.zone="$zone "-set podmonTest.image="$image" 
 done
--- test/podmontest/insv.sh
+++ test/podmontest/insv.sh
@@ -1,8 +1,9 @@
 #!/bin
-instances="1 2 3 4 5 6 7 8"
+instances="1 2 3 4"
 ndevices=0
 nvolumes=4
-storageClassName=vxflexos-notopo
+zone="zone213"
+storageClassName=vxflexos-retain
 image="$REGISTRY_HOST:$REGISTRY_PORT/podmontest:v0.0.54"
 port=5000
 
@@ -11,6 +12,6 @@ do
 	echo $i
 	kubectl create namespace pmtv$k
 	# following line for debug
-        #helm install --dry-run --debug -n "pmtv$i" "pmtv$i" deploy --values deploy/values-vxflex.yaml --set podmonTest.namespace="pmtv$i"  --set podmonTest.storageClassName="$storageClassName" --set podmonTest.image="$image" 
-        helm install -n "pmtv$i" "pmtv$i" deploy --values deploy/values-vxflex.yaml --set podmonTest.namespace="pmtv$i"  --set podmonTest.storageClassName="$storageClassName" --set podmonTest.ndevices=$ndevices --set podmonTest.nvolumes=$nvolumes --set podmonTest.image="$image" 
+        #helm install --dry-run --debug -n "pmtv$i" "pmtv$i" deploy --values deploy/values-vxflex.yaml --set podmonTest.namespace="pmtv$i"  --set podmonTest.storageClassName="$storageClassName" --set podmonTest.image="$image" --set podmonTest.zone="$zone"
+        helm install -n "pmtv$i" "pmtv$i" deploy --values deploy/values-vxflex.yaml --set podmonTest.namespace="pmtv$i"  --set podmonTest.storageClassName="$storageClassName" --set podmonTest.ndevices=$ndevices --set podmonTest.nvolumes=$nvolumes --set podmonTest.image="$image" --set podmonTest.zone="$zone"
 done
