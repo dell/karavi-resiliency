072756a68c43179ccf9fb9727f260782f9dd6277
rbo54 <thomas.watson@dell.com>
2022-01-20 12:20:08 -0500

    Add rebalance changes to nway test script; add bounce.kubelet as alternate way to kill node

--- test/sh/nway.sh
+++ test/sh/nway.sh
@@ -31,6 +31,7 @@ BOUNCEIPSECONDS=240             # Bounce IP time in seconds for interface down
 TIMEOUT=600			# Maximum time in seconds to wait for a failure cycle (needs to be higher than EVACUATE_TIMEOUT)
 MAXITERATIONS=3			# Maximum number of failover iterations
 DRIVERNS="vxflexos"		# Driver namespace
+REBALANCE=1			# Do rebalance if needed for pods with affinity
 
 rm -f stop			# Remove the stop file
 
@@ -56,6 +57,10 @@ for param in $*; do
     TIMEOUT=$1
     shift
     ;;
+  "--rebalance")
+    REBALANCE=1
+    shift
+    ;;
   "--help")
     shift
     echo "parameters: --ns driver-namespace [--bounceipseconds value] [--maxiterations value] [--timeoutseconds value]"
@@ -72,11 +77,89 @@ EVACUATE_TIMEOUT=$TIMEOUT       # Doesn't really matter if most of the time is s
 # and aborts if we exceed TIMEOUT
 check_timeout() {
 	if [ $1 -gt $TIMEOUT ]; then
-		echo "******************* timed out: " $1 "seconds ********************"
-		collect_logs.sh --ns $DRIVERNS
-		exit 2
+		if [ $REBALANCE -gt 0 ]; then
+			rebalance
+		else
+			echo "******************* timed out: " $1 "seconds ********************"
+			collect_logs.sh --ns $DRIVERNS
+			exit 2
+		fi
 	fi
 }
+
+# ====================================================================================================================================================
+# This part of the code rebalances pods across nodes for pod affinity.
+maxPods=90
+
+# nodelist returns a list of nodes(
+nodelist() {
+	kubectl get nodes -A | grep -v master  | grep -v NAME | awk '{ print $1 }'
+}
+
+# get the number of pods on a node $1
+podsOnNode() {
+	# Add an extra space on match string to differentiate worker-1 from worker-10
+	kubectl get pods -A -o wide | grep "$1 " | wc -l
+}
+
+# get namespaces of the pending pods 
+getNSOfPendingPods() {
+	 kubectl get pods -A -o wide | grep Pending | grep -v default | awk '{ print $1}'
+}
+
+# cordon a k8s node $1=node id
+cordon() {
+	echo "cordoning node $1"
+	kubectl cordon $1
+}
+
+# cordon a k8s node $1=node id
+uncordon() {
+	echo "uncordoning node $1"
+	kubectl uncordon $1
+}
+
+# delete pod names in namespace $1=namespace
+deletePodsInNS() {
+	pods=$(kubectl get pods -n $1 | grep -v NAME | awk '{print $1}')
+	echo pods "$pods to be deleted"
+	for pod in $pods; do
+		echo "kubctl delete pod -n $1 $pod"
+		kubectl delete pod --grace-period 0 -n $1 $pod
+	done
+}
+
+
+rebalance() {
+        echo "Rebalancing pods to nodes..."
+	cordonedNodes=""
+	nodes=$(nodelist)
+	echo nodes: $nodes
+	for n in $nodes; do
+		pods=$(podsOnNode $n)
+		echo node $n has $pods pods
+		if [ $pods -gt $maxPods ]; then
+			cordon $n
+			cordonedNodes="$cordonedNodes $n"
+		fi
+	done
+	echo cordonedNodes: $cordonedNodes
+	namespaces=$(getNSOfPendingPods)
+	for ns in $namespaces; do
+		echo "deleting pods in namespace $ns"
+		deletePodsInNS $ns
+	done
+	echo "waiting for pods to get moved"
+	for i in 1 2 3 4 5 6 7 8 9 10; do
+		kubectl get pods -l podmon.dellemc.com/driver -A -o wide | grep -v NAME | grep -v Running
+		sleep 60
+	done
+	for n in $cordonedNodes; do
+		uncordon $n
+	done
+	echo "Rebalancing complete"
+}
+# ====================================================================================================================================================
 
 
 print_status() {
