598b6db83a289a5ab2a139362241c3ecb8636821
Isaias-Martinez <Isaias_Martinez@dell.com>
2022-11-08 11:29:54 -0600

    added more doc headers + changes to format

--- internal/k8sapi/interface.go
+++ internal/k8sapi/interface.go
@@ -12,7 +12,7 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
- */
+*/
 
 // Package k8sapi provides an interface for access Kubernetes objects.
 package k8sapi
--- internal/utils/linuxLoopBackDevice.go
+++ internal/utils/linuxLoopBackDevice.go
@@ -2,7 +2,7 @@
 // +build test linux
 
 /*
-* Copyright (c) 2021-2022 Dell Inc., or its subsidiaries. All Rights Reserved.
+* Copyright (c) 2022 Dell Inc., or its subsidiaries. All Rights Reserved.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
- */
+*/
 
 package utils
 
