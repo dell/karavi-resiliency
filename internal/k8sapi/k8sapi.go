7c5fe1710dd607c1be2b197de3fd5e1aeee9eaa8
IsaiasA1 <109099794+IsaiasA1@users.noreply.github.com>
2022-11-09 10:33:29 -0600

    Doc update (#144)
    * added doc header to files
    
    * added more doc headers + changes to format
    
    * changes to format
    
    * changes to format
    
    * removed mention of ip and removal of line
    
    * removed xml files given from test
    
    * removal of ips hardcoded

--- internal/k8sapi/k8sapi.go
+++ internal/k8sapi/k8sapi.go
@@ -1,12 +1,17 @@
 /*
- * Copyright (c) 2021. Dell Inc., or its subsidiaries. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
+* Copyright (c) 2021-2022 Dell Inc., or its subsidiaries. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
  */
 
 package k8sapi
@@ -19,6 +24,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"sync"
+	"time"
+
 	log "github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	storagev1 "k8s.io/api/storage/v1"
@@ -33,8 +41,6 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/record"
-	"sync"
-	"time"
 )
 
 //Client holds a reference to a Kubernetes client
--- test/sh/basic.sh
+++ test/sh/basic.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-#
-# Copyright (c) 2021. Dell Inc., or its subsidiaries. All Rights Reserved.
+# Copyright (c) 2021-2022 Dell Inc., or its subsidiaries. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -8,7 +7,11 @@
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
-#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # This is a basic test that cycles through a set of listed nodes and kills them, making sure all the labeled apps continue to run after time for recovery.
 NODELIST=""
