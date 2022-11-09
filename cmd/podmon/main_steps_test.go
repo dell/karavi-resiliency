703a56457c6d36d745fc3879290ba3643bdab828
IsaiasA1 <109099794+IsaiasA1@users.noreply.github.com>
2022-11-09 10:33:29 -0600

    Doc update (#144)
    * added doc header to files
    
    * added more doc headers + changes to format
    
    * changes to format
    
    * changes to format
    
    * removed mention of ip and removal of line
    
    * removed xml files given from test
    
    * removal of ips hardcoded

--- cmd/podmon/main_steps_test.go
+++ cmd/podmon/main_steps_test.go
@@ -1,12 +1,17 @@
 /*
- * Copyright (c) 2021. Dell Inc., or its subsidiaries. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
+* Copyright (c) 2021-2022 Dell Inc., or its subsidiaries. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
  */
 
 package main
@@ -14,12 +19,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/cucumber/godog"
-	"github.com/dell/gofsutil"
-	logtest "github.com/sirupsen/logrus/hooks/test"
-	"github.com/stretchr/testify/assert"
-	"google.golang.org/grpc"
-	"k8s.io/client-go/kubernetes"
 	"os"
 	"podmon/internal/csiapi"
 	"podmon/internal/k8sapi"
@@ -27,6 +26,13 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/cucumber/godog"
+	"github.com/dell/gofsutil"
+	logtest "github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"k8s.io/client-go/kubernetes"
 )
 
 type mainFeature struct {
