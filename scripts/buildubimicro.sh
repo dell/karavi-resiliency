e93860040d5b5ea004618fb69a7c59e8f40c63ce
root <Isaias.Martinez@dell.com>
2023-07-07 09:31:41 -0400

    updated actions.yaml local

--- scripts/buildubimicro.sh
+++ scripts/buildubimicro.sh
@@ -18,4 +18,4 @@ micromount=$(buildah mount $microcontainer)
 dnf install --installroot $micromount --releasever=8 --nodocs --setopt install_weak_deps=false --setopt=reposdir=/etc/yum.repos.d/ openssl nettle gnutls systemd -y
 dnf clean all --installroot $micromount
 buildah umount $microcontainer
-buildah commit $microcontainer ${REGISTRY_HOST}:${REGISTRY_PORT}/resiliency-ubimicro
+buildah commit $microcontainer resiliency-ubimicro
