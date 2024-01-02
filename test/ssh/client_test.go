e1b6927df66327279de0370935e43f06a3cadab6
Luna Xu <ms.luna.xu@gmail.com>
2024-01-02 16:01:52 -0500

    fix issues

--- test/ssh/client_test.go
+++ test/ssh/client_test.go
@@ -18,12 +18,11 @@ package ssh_test
 
 import (
 	"fmt"
+	"podmon/test/ssh"
+	"podmon/test/ssh/mocks"
 	"strings"
 	"testing"
 	"time"
-
-	"podmon/test/ssh"
-	"podmon/test/ssh/mocks"
 
 	"github.com/golang/mock/gomock"
 )
