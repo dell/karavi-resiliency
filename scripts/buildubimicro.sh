63c6c0ef548a08294b865eb268bbdbb1d0431d8e
Aaron Tye <tyeaaron@gmail.com>
2024-02-01 12:54:27 -0500

    Update buildubimicro.sh repos (#187)
    * Update buildubimicro.sh
    
    * Update buildubimicro.sh
    
    * Update buildubimicro.sh

--- scripts/buildubimicro.sh
+++ scripts/buildubimicro.sh
@@ -15,7 +15,7 @@
 
 microcontainer=$(buildah from $1)
 micromount=$(buildah mount $microcontainer)
-dnf install --installroot $micromount --releasever=8 --nodocs --setopt install_weak_deps=false openssl nettle gnutls systemd -y
+dnf install --installroot $micromount --releasever=8 --nodocs --setopt install_weak_deps=false --setopt=reposdir=/etc/yum.repos.d/ openssl nettle gnutls systemd -y
 dnf clean all --installroot $micromount
 buildah umount $microcontainer
 buildah commit $microcontainer resiliency-ubimicro
