5c0a8f8f409b8d6c187c0a4dd2e181580f2b4d38
Konduri Satya Narayana <109734493+satyakonduri@users.noreply.github.com>
2025-04-14 11:24:50 +0530

    Update internal/monitor/controller.go
    Co-authored-by: lukeatdell <115811384+lukeatdell@users.noreply.github.com>

--- internal/monitor/controller.go
+++ internal/monitor/controller.go
@@ -280,8 +280,7 @@ func (cm *PodMonitorType) controllerCleanupPod(pod *v1.Pod, node *v1.Node, reaso
 		connected, iosInProgress, err := cm.callValidateVolumeHostConnectivity(node, volIDs, true)
 		log.WithFields(fields).Infof("Validating host connectivity for node %s volumes %v connected %t iosInProgress %t", node.ObjectMeta.Name, volIDs, connected, iosInProgress)
 		// If the volume's access mode is RWX, ignore iosInProgress, as other applications may perform I/O operations on the volume.
-		status := isRWXVolume(pvlist)
-		if status {
+		if isRWXVolume(pvlist) {
 			log.WithFields(fields).Info("Skipping iosInProgress check as the volume accessmode is RWX or ReadWriteMany")
 			iosInProgress = false
 		}
